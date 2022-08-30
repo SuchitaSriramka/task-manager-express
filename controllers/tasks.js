bf57013e56088ec2fd486916527f1286cb2e2495
SuchitaSriramka <suchitasriramka@gmail.com>
2022-08-30 20:30:14 +0530

    crud

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -48,7 +48,8 @@ const deleteTask = async (req, res) => {
 const updateTask = async (req, res) => {
     try {
         const {id:taskID} = req.params
-        res.status(200).json({id:taskID, data: req.body})
+        const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {new: true, runValidators: true})
+        res.status(200).json({task})
     } catch (error) {
         res.status(404).send('error')
     }
