22b82759fc245c8b8d82a2b22758c3b3ef942cff
SuchitaSriramka <suchitasriramka@gmail.com>
2022-08-30 18:17:33 +0530

    crud

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-    res.send('get all tasks')
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({tasks})
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const createTask = async (req, res) => {
