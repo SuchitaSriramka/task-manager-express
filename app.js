41d7b05fd3b7d5af971c3fbbbb33371a9b146954
SuchitaSriramka <suchitasriramka@gmail.com>
2022-08-30 11:07:39 +0530

    added basic routes

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
 
+// middleware
+app.use(express.json())
+
 //routes
+app.use('/api/v1/tasks', tasks)
+
 app.get('/hello', (req, res) => {
     res.send('Task Manager App - Hello')
 })
+
+
+
+// app.get('/api/v1/tasks') - get all the tasks
+// app.post('/api/v1/tasks') - create a new tasks
+// app.get('/api/v1/tasks/:id') - get single tasks
+// app.patch('api/v1/tasks:id') - update tasks
+// app.delete('/api/v1/tasks/:id') - delete task
 
 
 const port = 3000
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const {
+getAllTasks,
+createTask,
+updateTask,
+getTask,
+deleteTask} = require('../controllers/tasks')
 
-router.route('/').get((req, res) => {
-    res.send('hello')
-})
+router.route('/')
+.get(getAllTasks)
+.post(createTask)
+
+router.route('/:id')
+.get(getTask)
+.patch(updateTask)
+.delete(deleteTask)
 
 module.exports = router
