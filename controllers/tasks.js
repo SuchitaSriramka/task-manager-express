f79168478fb84fc2908b78f202597456ab438088
SuchitaSriramka <suchitasriramka@gmail.com>
2022-08-30 20:18:37 +0530

    crud

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -35,8 +35,17 @@ const updateTask = (req, res) => {
     res.send('update single task')
 }
 
-const deleteTask = (req, res) => {
-    res.send('delete single task')
+const deleteTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg: `${taskID} not found`})
+        }
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
 
 module.exports = {
